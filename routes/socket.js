88d85a297f7d360251c914f7f53d1145804519e9
Yuri Nikulin <kowkogab@gmail.com>
2018-02-16 16:33:12 +0400

    server and user messages have been separated via different namespaces in server part

--- routes/socket.js
+++ routes/socket.js
@@ -2,7 +2,19 @@ var io = require('socket.io');
 
 exports.initialize = function(server) {
 	io = io.listen(server);
-	io.sockets.on('connection', function(socket) {
+
+	var chatInfra = io.of("/chat_infra").on("connection", function(socket) {
+		socket.on("set_name", function(data) {
+			socket.username = data.name;
+			socket.emit('name_set', data);
+			socket.broadcast.send(JSON.stringify({
+				type: 'serverMessage',
+				message: '<em>' + data.name + '</em>' + ' has been connected'
+			}));
+		});
+	});
+
+	var chatCom = io.of("/chat_com").on("connection", function(socket) {
 		socket.on('message', function(message) {
 			message = JSON.parse(message);
 			message.author = socket.username;
@@ -12,14 +24,25 @@ exports.initialize = function(server) {
 				socket.send(JSON.stringify(message));
 			}
 		});
-
-		socket.on('set_name', function(data) {
-			socket.username = data.name;
-			socket.emit('name_set', data);
-			socket.broadcast.send(JSON.stringify({
-				type: 'serverMessage',
-				message: '<em>' + data.name + '</em>' + ' has been connected'
-			}))
-		})
 	});
+	// io.sockets.on('connection', function(socket) {
+	// 	socket.on('message', function(message) {
+	// 		message = JSON.parse(message);
+	// 		message.author = socket.username;
+	// 		if (message.type == "userMessage") {
+	// 			socket.broadcast.send(JSON.stringify(message));
+	// 			message.type = "myMessage";
+	// 			socket.send(JSON.stringify(message));
+	// 		}
+	// 	});
+
+	// 	socket.on('set_name', function(data) {
+	// 		socket.username = data.name;
+	// 		socket.emit('name_set', data);
+	// 		socket.broadcast.send(JSON.stringify({
+	// 			type: 'serverMessage',
+	// 			message: '<em>' + data.name + '</em>' + ' has been connected'
+	// 		}))
+	// 	})
+	// });
 };
