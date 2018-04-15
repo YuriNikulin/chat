38a1c99c86fe35b8b27ca85b7932abc394642b42
Yuri Nikulin <kowkogab@gmail.com>
2018-04-15 19:07:29 +0400

    bandwidth testing

--- public/js/webrtc.js
+++ public/js/webrtc.js
@@ -43,8 +43,8 @@ function setBandwidth(sdp) {
 }
 
 function setBandwidth(sdp) {
-	var audioBandwidth = 20;
-	var videoBandwidth = 100;
+	var audioBandwidth = 30;
+	var videoBandwidth = 256;
 	
     sdp = sdp.replace(/a=mid:audio\r\n/g, 'a=mid:audio\r\nb=AS:' + audioBandwidth + '\r\n');
     sdp = sdp.replace(/a=mid:video\r\n/g, 'a=mid:video\r\nb=AS:' + videoBandwidth + '\r\n');
