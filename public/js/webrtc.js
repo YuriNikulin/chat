9acea2e63c402649ea96c448220c8fdc4935b60a
Yuri Nikulin <kowkogab@gmail.com>
2018-04-28 14:22:29 +0400

    test

--- public/js/webrtc.js
+++ public/js/webrtc.js
@@ -3,7 +3,7 @@ var webrtcObj = {},
 	mainVideoContainer = document.querySelector('.cr-video-main'),
 	webrtcUsers = {};
 webrtcObj.config = {
-	'iceServers': [{ "url": "stun:stun.12connect.com:3478" }]
+	'iceServers': [{ "url": "stun:stun.l.google.com:19302" }]
 };
 webrtcConstraints = {
 	width: {
@@ -57,7 +57,6 @@ function WebRTCUser(user) {
 	var self = this;
 	this.stream = webrtcObj.stream;
 	this.pc = new RTCPeerConnection(webrtcObj.config);
-	debugger;
 	this.pc.onicecandidate = function(event) {
 		if (event.candidate) {
 			webrtcMsg(currentUser, self.wid, {
