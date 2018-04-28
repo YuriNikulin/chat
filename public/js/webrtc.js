c7552a0e34030dc8e2a8f3a7ffb4e8c9747af28c
Yuri Nikulin <kowkogab@gmail.com>
2018-04-28 14:47:47 +0400

    test

--- public/js/webrtc.js
+++ public/js/webrtc.js
@@ -3,7 +3,11 @@ var webrtcObj = {},
 	mainVideoContainer = document.querySelector('.cr-video-main'),
 	webrtcUsers = {};
 webrtcObj.config = {
-	'iceServers': [{ "url": "stun:stun.l.google.com:19302" }]
+	'iceServers': [{ 
+		url: 'turn:192.158.29.39:3478?transport=udp',
+		credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+		username: '28224511:1379330808'
+	}]
 };
 webrtcConstraints = {
 	width: {
